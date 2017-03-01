2ebdba89eb0c7e0dd44bc503fa8857ed7fe4b5b0
lazywithclass <montecristoh@gmail.com>
2017-02-28 19:12:09 -0500

    cleaned, still have to see what goes in here

--- src/cerebro/core.cljs
+++ src/cerebro/core.cljs
@@ -1,13 +1,2 @@
 (ns cerebro.core
-  (:require [cljs.nodejs :as node]
-            [cerebro.mutations-reader :as reader]))
-
-
-
-(node/enable-util-print!)
-
-(println (reader/answer))
-
-;; (println reader)
-
-;; (set! (.-exports js/module) Cerebro)
+  (:require [cljs.nodejs :as node]))
