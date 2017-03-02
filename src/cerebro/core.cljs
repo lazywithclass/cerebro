d29f864ed750ca0e93cda31ca19921ea2c0cdeeb
lazywithclass <montecristoh@gmail.com>
2017-03-02 13:30:57 -0500

    runner

--- src/cerebro/core.cljs
+++ src/cerebro/core.cljs
@@ -1,2 +1,29 @@
 (ns cerebro.core
-  (:require [cljs.nodejs :as node]))
+  (:require [cljs.nodejs :as node]
+            [cerebro.mutations.true-to-false :as true-to-false]
+            [cerebro.lib.utils :as utils]
+            [cerebro.lib.vm-mocha :as vm-mocha]
+            [cerebro.lib.source-reader :as reader]))
+
+;; TODO dont mutate code that is not covered!
+
+(defn run
+  "runs the example"
+  []
+  (let [;; read source
+        source (first (reader/to-ast (reader/read "./test/example-project/lib")))
+        ;; read test
+        test (first (vals (first (reader/to-ast (reader/read "./test/example-project/test")))))
+        ;; apply the mutation
+        mutated (true-to-false/loop-nodes source)
+        code (str
+              (vm-mocha/ast-to-string mutated)
+              (vm-mocha/ast-to-string test)
+              "mocha.run()")]
+    (prn code)
+    (vm-mocha/mutant-killed? (vm-mocha/run-in-context code (vm-mocha/create-context)))))
+
+;; run tests
+;; TODO fails because `check` has already been declared
+(run)
+
