91ae9da7636092c43e8f4f1e23b86d22bdd0ab52
lazywithclass <montecristoh@gmail.com>
2017-03-30 15:13:32 -0400

    added a function that pretty prints

--- src/cerebro/lib/utils.cljs
+++ src/cerebro/lib/utils.cljs
@@ -5,3 +5,8 @@
   "stringifies using JSON.stringify"
   [object]
   (.stringify js/JSON object))
+
+(defn log-stringified
+  "prints with proper stringification"
+  [object]
+  (.log js/console (.stringify js/JSON object nil "  ")))
